1c4cf2ee1221ebe19547e922403bf2ab494942f7
Aleksandr Razumov <ernado@go-faster.org>
2023-09-24 20:38:28 +0300

    fix: use minikube

--- up.sh
+++ up.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-kind create cluster --config=kind-config.yaml
-docker pull quay.io/cilium/cilium:v1.14.2
-kind load docker-image quay.io/cilium/cilium:v1.14.2
+minikube start --nodes 2 --driver kvm2 --container-runtime=containerd  --network-plugin=cni --cni=false --cpus 8
 
-helm install cilium cilium/cilium --version 1.14.2 \
-   --namespace kube-system \
+helm upgrade --install --version 1.14.2 \
+   --namespace cilium --create-namespace \
    --set image.pullPolicy=IfNotPresent \
-   --set ipam.mode=kubernetes
-helm upgrade --install -n kube-system --values tetragon.yml tetragon cilium/tetragon
+   --set ipam.mode=kubernetes \
+   cilium cilium/cilium
+
+helm upgrade --install \
+  --namespace cilium --create-namespace \
+  --values tetragon.yml \
+  tetragon cilium/tetragon
 
 cilium status --wait
