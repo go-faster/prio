3e332e7bf1c3ae41e145c889fa72c2a0fadf3e68
Aleksandr Razumov <ernado@go-faster.org>
2023-09-24 22:09:13 +0300

    feat: upd

--- up.sh
+++ up.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-minikube start --nodes 2 --driver kvm2 --container-runtime=containerd  --network-plugin=cni --cni=false --cpus 8
+set -ex
 
-helm upgrade --install --version 1.14.2 \
-   --namespace cilium --create-namespace \
-   --set image.pullPolicy=IfNotPresent \
-   --set ipam.mode=kubernetes \
-   cilium cilium/cilium
+minikube start --nodes 2 --driver kvm2 --cpus 8
 
 helm upgrade --install \
-  --namespace cilium --create-namespace \
+  --namespace tetragon --create-namespace \
   --values tetragon.yml \
   tetragon cilium/tetragon
 
-cilium status --wait
