6726b6d8acf6ba2436235f627456596e50bb3c07
Aleksandr Razumov <ernado@go-faster.org>
2023-09-25 18:35:31 +0300

    chore: update up.sh
    * Use URL from github releases
    * Install applications

--- up.sh
+++ up.sh
@@ -2,10 +2,11 @@
 
 set -ex
 
-minikube start --iso-url=file:///src/minikube/out/minikube-amd64.iso --nodes 2 --driver kvm2 --cpus 8
+minikube start --iso-url=https://github.com/go-faster/minikube/releases/download/v1.31.2-5.15/minikube-amd64.iso --nodes 2 --driver kvm2 --cpus 8
 
 helm upgrade --install \
   --namespace tetragon --create-namespace \
   --values tetragon.yml \
   tetragon cilium/tetragon
 
+make priod example
